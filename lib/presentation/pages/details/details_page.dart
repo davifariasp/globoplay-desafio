a70a0073745b22e90d7fcb80847c339c6604f78a
davifariasp <davi.informatica8@gmail.com>
2024-03-26 16:52:40 -0300

    adicionando imagem, bakcdrop, texto e botoes a pagina de detalhes

--- lib/presentation/pages/details/details_page.dart
+++ lib/presentation/pages/details/details_page.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:globoplay_mobile/data/http/http_client.dart';
+import 'package:globoplay_mobile/data/repositories/midia_repository.dart';
+import 'package:globoplay_mobile/presentation/pages/details/stores/midia_store.dart';
+import 'package:globoplay_mobile/utils.dart';
 
 class DetailsPage extends StatefulWidget {
   final int id;
@@ -9,14 +15,174 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
+  final MidiaStore midiaStore = MidiaStore(
+    repository: MidiaRepository(
+      client: HttpClient(),
+    ),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    midiaStore.getMidia(widget.id);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Detalhes'),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: white,
+      ),
+      body: AnimatedBuilder(
+        animation: Listenable.merge(
+          [
+            midiaStore.isLoading,
+            midiaStore.error,
+            midiaStore.midia,
+          ],
         ),
-        body: Center(
-          child: Text(widget.id.toString()),
-        ));
+        builder: (context, child) {
+          if (midiaStore.isLoading.value) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (midiaStore.midia.value != null) {
+            return Container(
+                height: size.height,
+                width: size.width,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      '$urlImgAPI${midiaStore.midia.value.backdropPath}',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 80,
+                          ),
+                          SizedBox(
+                            width: 180,
+                            child: Image.network(
+                              '$urlImgAPI${midiaStore.midia.value.posterPath}',
+                              fit: BoxFit.contain,
+                              errorBuilder: (context, error, stackTrace) =>
+                                  const Icon(Icons.error),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            midiaStore.midia.value.name ?? '',
+                            style: const TextStyle(
+                              color: white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            midiaStore.midia.value.overview ?? '',
+                            style: const TextStyle(
+                              color: white,
+                              fontSize: 18,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              TextButton.icon(
+                                onPressed: () {},
+                                icon: const Icon(Icons.play_arrow),
+                                label: const Text(
+                                  'Assistir',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    white,
+                                  ),
+                                  foregroundColor: MaterialStateProperty.all(
+                                    grey,
+                                  ),
+                                  fixedSize: MaterialStateProperty.all(
+                                    const Size(180, 50),
+                                  ),
+                                  overlayColor: MaterialStateProperty.all(
+                                    greyBt,
+                                  ),
+                                  shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              TextButton.icon(
+                                onPressed: () {},
+                                icon: const Icon(Icons.star),
+                                label: const Text(
+                                  'Minha lista',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                style: ButtonStyle(
+                                  foregroundColor: MaterialStateProperty.all(
+                                    greyIcon,
+                                  ),
+                                  fixedSize: MaterialStateProperty.all(
+                                    const Size(180, 50),
+                                  ),
+                                  overlayColor: MaterialStateProperty.all(
+                                    white,
+                                  ),
+                                  shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                      side: const BorderSide(color: greyIcon),
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ));
+          } else {
+            return const Center(
+              child: Text(
+                "Nenhuma midia encontrada!",
+                style: TextStyle(color: white),
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
---
+++ lib/presentation/pages/details/stores/midia_store.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:globoplay_mobile/data/http/exceptions.dart';
+import 'package:globoplay_mobile/data/repositories/midia_repository.dart';
+import 'package:globoplay_mobile/domain/models/midia.dart';
+
+class MidiaStore {
+  final IMidiaRepository repository;
+
+  // Variavel reativa para o loading
+  final ValueNotifier<bool> isLoading = ValueNotifier<bool>(false);
+
+  // Variavel reativa para o state
+  final ValueNotifier<Midia> midia = ValueNotifier<Midia>(Midia());
+
+  // Variavel reativa para o erro
+  final ValueNotifier<String> error = ValueNotifier<String>('');
+
+  MidiaStore({required this.repository});
+
+  getMidia(int id) async {
+    isLoading.value = true;
+
+    try {
+      final result = await repository.getMidiabyId(id);
+      midia.value = result;
+    } on NotFoundException catch (e) {
+      debugPrint('aqui 1' + e.toString());
+      error.value = e.message;
+    } catch (e) {
+      debugPrint('aqui 2' + e.toString());
+      error.value = 'Erro ao carregar midias';
+    }
+
+    isLoading.value = false;
+  }
+}
