a0ce1a03dc4490a194b0ddd8265239fc8ca00635
davifariasp <davi.informatica8@gmail.com>
2024-03-27 16:05:31 -0300

    consumindo api para pegar casting e recomendacoes

--- lib/data/repositories/midia_repository.dart
+++ lib/data/repositories/midia_repository.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:globoplay_mobile/data/http/http_client.dart';
 import 'package:globoplay_mobile/domain/models/midia.dart';
 
@@ -7,6 +8,8 @@ abstract class IMidiaRepository {
   getSeries();
   getMovies();
   getMidiabyId(int id); //more details
+  getCast(int id);
+  getRecomendacoes(int id);
 }
 
 class MidiaRepository implements IMidiaRepository {
@@ -69,11 +72,17 @@ class MidiaRepository implements IMidiaRepository {
             'https://api.themoviedb.org/3/tv/$id?language=pt-BR&api_key=f5584caeecdbc0937682e809bacb43c4');
 
     if (response.statusCode == 200) {
-
       final body = jsonDecode(response.body);
       final Midia midia = Midia.fromMap(body);
       return midia;
-      
+    } else if (response.statusCode == 404) {
+      final newResponse = await client.get(
+          url:
+              'https://api.themoviedb.org/3/movie/$id?language=pt-BR&api_key=f5584caeecdbc0937682e809bacb43c4');
+
+      final body = jsonDecode(newResponse.body);
+      final Midia midia = Midia.fromMap(body);
+      return midia;
     } else {
       throw Exception('Erro ao carregar midia');
     }
@@ -93,13 +102,73 @@ class MidiaRepository implements IMidiaRepository {
 
       body['results'].map((item) {
         final Midia midia = Midia.fromMap(item);
+        debugPrint(midia.toString());
+        midias.add(midia);
+      }).toString();
+
+      return midias;
+    } else {
+      throw Exception('Erro ao carregar filmes');
+    }
+  }
+
+  @override
+  getCast(int id) async {
+    final response = await client.get(
+        url:
+            'https://api.themoviedb.org/3/tv/$id/aggregate_credits?language=pt-br&api_key=f5584caeecdbc0937682e809bacb43c4');
+
+    if (response.statusCode == 200) {
+      List<String> cast = [];
+
+      final body = jsonDecode(response.body);
+
+      body['cast'].map((item) {
+        cast.add(item['name']);
+      }).toString();
+
+      return cast;
+    } else {
+      throw Exception('Erro ao carregar casting');
+    }
+  }
+
+  @override
+  getRecomendacoes(int id) async {
+    final response = await client.get(
+        url:
+            'https://api.themoviedb.org/3/tv/$id/recommendations?language=pt-BR&api_key=f5584caeecdbc0937682e809bacb43c4');
+
+    if (response.statusCode == 200) {
+      List<Midia> midias = [];
+
+      final body = jsonDecode(response.body);
+      //debugPrint(body['results'].toString());
+
+      body['results'].map((item) {
+        final Midia midia = Midia.fromMap(item);
+        //debugPrint(midia.toString());
+        midias.add(midia);
+      }).toString();
+
+      return midias;
+    } else if (response.statusCode == 404) {
+      final newResponse = await client.get(
+          url:
+              'https://api.themoviedb.org/3/movie/$id/recommendations?language=pt-BR&api_key=f5584caeecdbc0937682e809bacb43c4');
+
+      List<Midia> midias = [];
+
+      final body = jsonDecode(newResponse.body);
+      body['results'].map((item) {
+        final Midia midia = Midia.fromMap(item);
         //debugPrint(midia.toString());
         midias.add(midia);
       }).toString();
 
       return midias;
     } else {
-      throw Exception('Erro ao carregar midias');
+      throw Exception('Erro ao carregar recomendacoes');
     }
   }
 }
